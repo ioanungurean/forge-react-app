97746f13b19c73b92515bcf3001509068be91eea
Ioan Ungurean <inungurean@gmail.com>
2017-11-26 15:06:39 +0200

    Add custom error reporter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import Redbox from 'redbox-react';
 import { AppContainer } from 'react-hot-loader';
-
-import 'ui/layout';
 
 import App from 'modules/app';
 
+import 'ui/layout';
+
+const CustomErrorReporter = ({ error }) => <Redbox error={error} />;
+
+CustomErrorReporter.propTypes = {
+  error: PropTypes.instanceOf(Error).isRequired,
+};
+
 const render = (Component) => {
   ReactDOM.render(
-    <AppContainer>
+    <AppContainer errorReporter={CustomErrorReporter} warnings={false}>
       <Component />
     </AppContainer>,
     document.getElementById('root'),
