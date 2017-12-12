b235ea131328fb6b24b8d1bcdac84d24869deff3
Ioan Ungurean <inungurean@gmail.com>
2017-12-12 21:21:55 +0200

    Removing the custom error reporting tool redbox-react

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
-import Redbox from 'redbox-react';
 import { AppContainer } from 'react-hot-loader';
 
 import App from 'modules/app';
 
 import 'gui/layout';
 
-const CustomErrorReporter = ({ error }) => <Redbox error={error} />;
-
-CustomErrorReporter.propTypes = {
-  error: PropTypes.instanceOf(Error).isRequired,
-};
-
 const render = (Component) => {
   ReactDOM.render(
-    <AppContainer errorReporter={CustomErrorReporter} warnings={false}>
+    <AppContainer>
       <Component />
     </AppContainer>,
     document.getElementById('root'),
