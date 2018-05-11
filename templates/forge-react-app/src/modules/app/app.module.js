fa86b1524220160120e3904699f2c07d309a711e
Ioan Ungurean <inungurean@gmail.com>
2018-05-12 01:13:20 +0200

    Clean boilerplate and add small improvements

--- templates/forge-react-app/src/modules/app/app.module.js
+++ templates/forge-react-app/src/modules/app/app.module.js
@@ -4,13 +4,12 @@ import { hot } from 'react-hot-loader';
 import Title from 'components/title/title.component';
 
 import './app.style.scss';
-import cover from 'gui/assets/images/cover.svg';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <img className="app__cover" src={cover} alt="Forge React App"/>
+        <div className="app__cover" />
         <a className="app__link"href="https://github.com/ioanungurean/forge-react-app">
           <Title title="Forge React App" />
         </a>
